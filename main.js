65533cc91c8ba6fc8e33b892d40a5725d0d92612
Isabelafethx <151685915+Isabelafethx@users.noreply.github.com>
2024-04-09 19:53:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 song = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
@@ -25,6 +27,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
@@ -40,6 +46,50 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+ if(scoreRightWrist > 0.2)
+ { 
+    circle(rightWristX,rightWristY,20);
+ 
+
+ if(rightWristY >0 && rightWristY <= 100)
+    {
+        document.getElementById("speed").innerHTML = "Velocidade = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY >100 && rightWristY <=200)
+    {
+        document.getElementById("speed").innerHTML = "Velocidade = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY >200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "Velocidade = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY >300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "Velocidade = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY >400)
+    {
+        document.getElementById("speed").innerHTML = "Velocidade = 2.5x";
+        song.rate(2.5);
+    }
+ }
+
+if(scoreLeftWrist > 0.2)
+{
+    circle(leftWristX, leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimais = floor(InNumberLeftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
+ }
 }
 
 function play()
